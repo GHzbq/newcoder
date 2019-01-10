061814c9a6dd703c5fd58aba0f337ee89f3c37a7
GHzbq <eric_qiushui@126.com>
2019-01-10 15:36:31 +0800

    2019-1-10

---
+++ FindPath.cpp
@@ -0,0 +1,34 @@
+/* 输入一颗二叉树的跟节点和一个整数，
+ * 打印出二叉树中结点值的和为输入整数的所有路径。
+ * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+ * (注意: 在返回值的list中，数组长度大的数组靠前) 
+ * */
+
+#include <iostream>
+#include <vector>
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    std::vector<std::vector<int> > FindPath(TreeNode* root,int expectNumber) {
+        
+    }
+};
