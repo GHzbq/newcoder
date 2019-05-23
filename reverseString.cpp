2ad47923421c5f1ae1aeb0a2c900d50952135b62
GHzbq <eric_qiushui@126.com>
2019-05-23 10:01:45 +0800

    update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,28 +1,40 @@
-/* 翻转字符串 */
-
 #include <iostream>
+#include <algorithm>
 #include <string>
 
-class Solution
+void Reverse(std::string& str)
 {
-public:
-    std::string reverseString(std::string s)
+    if(str.empty())
     {
-        if(s.empty())
-        {
-            return s;
-        }
-
-        size_t begin = 0;
-        size_t end = s.size() - 1;
-        while(begin < end)
-        {
-            std::swap(s[begin], s[end]);
-            ++begin;
-            --end;
-        }
-
-        return s;
+        return;
     }
     
-};
+    auto start = str.begin();
+    auto end = str.begin();
+    for(; end != str.end(); ++end)
+    {
+        if(*end == ' ' && start != end)
+        {
+            std::reverse(start, end);
+            start = end + 1;
+        }
+    }
+    // 对最后一个字符串 有点问题
+    if(start != str.end())
+    {
+        std::reverse(start, end);
+    }
+
+    // 再对整个字符串翻转
+    std::reverse(str.begin(), str.end());
+}
+
+int main(void)
+{
+    std::string str(100, '\0');
+    while(std::getline(std::cin, str))
+    {
+        Reverse(str);
+        std::cout << str << std::endl;
+    }
+}
